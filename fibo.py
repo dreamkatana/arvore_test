48b8ac65de69a9ecd60879a8e3917986748c3b3d
Joao Eduardo <joaoedu@gmail.com>
2023-04-02 21:18:03 -0300

    Ramifica 1 time

--- fibo.py
+++ fibo.py
@@ -1,3 +1,7 @@
+import time
+import sys
+sys.setrecursionlimit(5000)
+
 def fibonacci(n):
     if n <= 0:
         return 0
@@ -6,4 +10,7 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
   
+start = time.time()    
 print(fibonacci(20))
+end = time.time()
+print(end - start)
